093c5a8b4b5dc09162b982a27210e97a1d8804b0
ibrar76 <155563149+ibrar76@users.noreply.github.com>
2024-01-10 09:14:26 -0800

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,12 @@
+while True:
+    marks = int(input('enter your marks='))
+    if marks > 90:
+        print('Grade A')
+    elif marks > 80 and marks < 90:
+        print('Grade B')
+    elif marks > 70 and marks < 80:
+        print('Grade C')
+    elif marks > 60 and marks < 70:
+        print('Grade D')
+    else:
+        print('Fail')
